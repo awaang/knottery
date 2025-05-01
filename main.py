b9ff23d41049dc58c4f85051c5d372d85e214fd2
Amy Wang <ayw@umail.ucsb.edu>
2025-05-01 16:39:23 -0700

    rewrote main.py criteria logic

--- main.py
+++ main.py
@@ -7,21 +7,19 @@ from knot.non_alternating_knot import NonAlternatingKnot
 # - Check only the permutations that are not self contained
 # - Check each "tree" from each dowker code
 # - Use sorting algorithm to determine lexographically minimal code
-# - Eliminate self contained list of flypes from permutations, add lexographically minimal code to a list (Final list)
+# - Eliminate self contained list of flypes from permutations, add lexographically minimal code to final_list
 
-# - at 8 crossings, there are 17 non prime knots included atm
+# - at 8 crossings, there are 17 non prime knots included atm ????????
 
 def gen_alternating_knots():
      permutations = gen_dowkers(8)
-     
-     for i in range(len(permutations)):
-          if AlternatingKnot(permutations[i]).is_lexographic() == False: # criteria 1: checks if the dowker code is lexographically minimal
-               permutations[i] = 0
-          elif AlternatingKnot(permutations[i]).is_prime() == False: # criteria 2: checks if the dowker code is prime
-               permutations[i] = 0 
-          elif AlternatingKnot(permutations[i]).is_possible() == False: # criteria 3: checks if the dowker code is possible
-               permutations[i] = 0
-     permutations = AlternatingKnot(permutations).zero_remove() # remove all zeroed permutations
+
+     permutations = [
+          p for p in permutations
+          if AlternatingKnot(p).is_lexographic() # criteria 1: checks if the dowker code is lexographically minimal
+          and AlternatingKnot(p).is_prime() # criteria 2: checks if the dowker code is prime
+          and AlternatingKnot(p).is_possible() # criteria 3: checks if the dowker code is possible
+     ]
 
      permutations = AlternatingKnot.compute_flype_minimals(permutations) # criteria 4: checks if the dowker code is minimal with respect to flyping
 
